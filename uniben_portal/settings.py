d0e72839796cb203089655a106addfee8b257f70
Israel-Irene-Idemudia <israelireneidemudia@gmail.com>
2025-09-25 05:06:51 +0100

    update on servers

--- uniben_portal/settings.py
+++ uniben_portal/settings.py
@@ -131,6 +131,13 @@ DATABASES = {
     )
 }
 """
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
