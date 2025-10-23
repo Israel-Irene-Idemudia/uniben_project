26aa8fb5ab454f19c862d1bb1a8378c0af51f777
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-10-23 09:42:57 +0000

    coursses touches reverted

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Faculty, Department, CourseArea, Level, Course
+from .models import Faculty, Department, Level, Course
+
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ("name",)
@@ -9,14 +10,9 @@ class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("name", "faculty")
     list_filter = ("faculty",)
 
-@admin.register(CourseArea)
-class CourseAreaAdmin(admin.ModelAdmin):
-    list_display = ("name", "department")
-    list_filter = ("department",)
-
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
-    list_display = ("name", "department", "course_area")
+    list_display = ("name", "department")
     list_filter = ("department",)
 
 @admin.register(Course)
