ec884bd5000e8da69b3bd9e66fbcf9d40dfe3ef1
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-10-23 10:36:30 +0000

    Revert "coursses touches reverted"
    This reverts commit 26aa8fb5ab454f19c862d1bb1a8378c0af51f777.

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Faculty, Department, Level, Course
-
+from .models import Faculty, Department, CourseArea, Level, Course
 @admin.register(Faculty)
 class FacultyAdmin(admin.ModelAdmin):
     list_display = ("name",)
@@ -10,9 +9,14 @@ class DepartmentAdmin(admin.ModelAdmin):
     list_display = ("name", "faculty")
     list_filter = ("faculty",)
 
+@admin.register(CourseArea)
+class CourseAreaAdmin(admin.ModelAdmin):
+    list_display = ("name", "department")
+    list_filter = ("department",)
+
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
-    list_display = ("name", "department")
+    list_display = ("name", "department", "course_area")
     list_filter = ("department",)
 
 @admin.register(Course)
