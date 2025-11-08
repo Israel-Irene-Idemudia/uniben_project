0fd1fe224b0f93de6d7152f63603cd1401fbf6f1
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-11-08 23:40:27 +0000

    Fix: Corrected level filtering logic in LevelViewSet

--- core/views.py
+++ core/views.py
@@ -55,27 +55,18 @@ class LevelViewSet(ReadOnlyModelViewSet):
         Dynamically filters levels based on faculty, department, and course area.
         - If `department_id` is provided, it filters levels for that department.
         - If `course_area_id` is also provided, it further refines the filter.
-        - This allows the frontend to get a context-specific list of levels.
         """
         qs = Level.objects.all()
 
-        # Get filter parameters from the request
         department_id = self.request.query_params.get('department_id')
         course_area_id = self.request.query_params.get('course_area_id')
 
-        # A level is linked to a department and optionally to a course area.
-        # So, the primary filter is the department.
         if department_id:
             qs = qs.filter(department_id=department_id)
 
             # If a course area is also specified, filter by that as well.
-            # If not, we should only return levels that DON'T have a course area.
             if course_area_id:
                 qs = qs.filter(course_area_id=course_area_id)
-            else:
-                # This ensures we don't accidentally return levels that are 
-                # specific to a course area.
-                qs = qs.filter(course_area__isnull=True)
 
         else:
             # If no department is specified, it doesn't make sense to return any levels.
@@ -98,11 +89,9 @@ class CourseViewSet(ReadOnlyModelViewSet):
         department_id = params.get('department_id')
         level_id = params.get('level_id')
 
-        # We must have a department and a level to find courses
         if not all([department_id, level_id]):
             return qs.none()
 
-        # --- Build the filter using IDs ---
         final_qs = qs.filter(
             level__department_id=department_id,
             level_id=level_id
@@ -112,8 +101,6 @@ class CourseViewSet(ReadOnlyModelViewSet):
         if course_area_id:
             final_qs = final_qs.filter(level__course_area_id=course_area_id)
         else:
-            # If no course area, only get courses where the level isn't 
-            # linked to a course area.
             final_qs = final_qs.filter(level__course_area__isnull=True)
 
         return final_qs
