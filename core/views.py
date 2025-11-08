ade397d989bdd9c5a5eb092d00f16d81820dd4c7
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-11-08 06:32:45 +0000

    updated backend for dynamic data loading

--- core/views.py
+++ core/views.py
@@ -9,7 +9,7 @@ from core.serializers import (
     FacultySerializer, 
     DepartmentSerializer, 
     CourseAreaSerializer, 
-    LevelSerializer,  # Import LevelSerializer
+    LevelSerializer,
     CourseSerializer
 )
 
@@ -46,21 +46,43 @@ class CourseAreaViewSet(ReadOnlyModelViewSet):
         return qs
 
 
-# --- NEW: ViewSet for Levels ---
 class LevelViewSet(ReadOnlyModelViewSet):
     serializer_class = LevelSerializer
     permission_classes = [AllowAny]
 
     def get_queryset(self):
-        # Returns all levels, optionally filtered by department
+        """
+        Dynamically filters levels based on faculty, department, and course area.
+        - If `department_id` is provided, it filters levels for that department.
+        - If `course_area_id` is also provided, it further refines the filter.
+        - This allows the frontend to get a context-specific list of levels.
+        """
         qs = Level.objects.all()
+
+        # Get filter parameters from the request
         department_id = self.request.query_params.get('department_id')
+        course_area_id = self.request.query_params.get('course_area_id')
+
+        # A level is linked to a department and optionally to a course area.
+        # So, the primary filter is the department.
         if department_id:
             qs = qs.filter(department_id=department_id)
+
+            # If a course area is also specified, filter by that as well.
+            # If not, we should only return levels that DON'T have a course area.
+            if course_area_id:
+                qs = qs.filter(course_area_id=course_area_id)
+            else:
+                # This ensures we don't accidentally return levels that are 
+                # specific to a course area.
+                qs = qs.filter(course_area__isnull=True)
+
+        else:
+            # If no department is specified, it doesn't make sense to return any levels.
+            return qs.none()
+
         return qs.order_by('name')
 
-
-# --- CourseViewSet using IDs ---
 
 class CourseViewSet(ReadOnlyModelViewSet):
     serializer_class = CourseSerializer
@@ -68,29 +90,30 @@ class CourseViewSet(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         qs = Course.objects.select_related(
-            'level', 'level__department', 'level__department__faculty', 'level__course_area'
+            'level', 'level__department', 'level__course_area'
         ).order_by('code', 'id')
 
         params = self.request.query_params
 
-        faculty_id = params.get('faculty_id')
         department_id = params.get('department_id')
-        level_id = params.get('level_id') # Changed from level_param to level_id
+        level_id = params.get('level_id')
 
-        if not all([faculty_id, department_id, level_id]):
+        # We must have a department and a level to find courses
+        if not all([department_id, level_id]):
             return qs.none()
 
         # --- Build the filter using IDs ---
         final_qs = qs.filter(
-            level__department__faculty_id=faculty_id,
             level__department_id=department_id,
-            level_id=level_id # Use the level ID directly
+            level_id=level_id
         )
 
         course_area_id = params.get('course_area_id')
         if course_area_id:
             final_qs = final_qs.filter(level__course_area_id=course_area_id)
         else:
+            # If no course area, only get courses where the level isn't 
+            # linked to a course area.
             final_qs = final_qs.filter(level__course_area__isnull=True)
 
         return final_qs
