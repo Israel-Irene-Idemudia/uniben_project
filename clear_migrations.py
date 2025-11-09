b62a8024d14dcb25527d4a1007c139421c463164
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-11-09 04:00:23 +0000

    v

--- clear_migrations.py
+++ clear_migrations.py
@@ -1,10 +1,13 @@
 import os
 
-apps = ["core", "cbt"]  # add all apps that have migrations
+apps = ["accounts", "api", "cbt", "core", "events", "materials", "news"]  # add all apps that have migrations
 
 for app in apps:
     migrations_path = os.path.join(app, "migrations")
+    if not os.path.exists(migrations_path):
+        continue
     for f in os.listdir(migrations_path):
         if f.endswith(".py") and f != "__init__.py":
             os.remove(os.path.join(migrations_path, f))
+            print(f"🗑️ Removed migration file: {f} from {app}")
     print(f"✅ Cleared migrations for {app}")
