9d81e418352b492d6231a1eda30b20119cfee9c9
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-11-11 09:54:08 +0000

    update to semester

--- core/serializers.py
+++ core/serializers.py
@@ -28,7 +28,7 @@ class CourseSerializer(serializers.ModelSerializer):
     # Rename fields to match the Flutter app's expectations
     level = serializers.CharField(source='level.name', read_only=True)
     department = serializers.CharField(source='level.department.name', read_only=True)
-    faculty = serializers.CharField(source='level.department.faculty.name', read_nowrite_filely=True)
+    faculty = serializers.CharField(source='level.department.faculty.name', read_only=True)
     course_area = serializers.CharField(source='level.course_area.name', read_only=True, allow_null=True)
 
     # Rename 'code' and 'title' for consistency
