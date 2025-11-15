79db8b16764ab93c1127d0864ab877b7a2012fca
chiazor.oreoluwa17 <chiazor.oreoluwa17@gmail.com>
2025-11-15 03:54:06 +0000

    llllllll

--- core/migrations/0003_remove_course_uniq_course_code_per_level_and_more.py
+++ core/migrations/0003_remove_course_uniq_course_code_per_level_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0002_add_unique_constraints'),
+        ('core', '0001_initial'),
     ]
 
     operations = [
