2872a83841b2d8abe3e1878a60efb0a91431de76
Oreoluwa Ifedinma Chiazor <Israel-Irene-Idemudia@users.noreply.github.com>
2026-01-30 15:56:01 +0100

    fixed name error

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, permissions
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import RegisterSerializer, UserProfileSerializer, UpdateProfileSerializer
 from .models import UserProfile
